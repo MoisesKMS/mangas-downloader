ce3b386f10a33de279d341940049f24cb221a41b
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-09-30 19:05:17 -0500

    update
    Agregado titulo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
-    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.41 YaBrowser/21.5.0.582 Yowser/2.5 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.41 YaBrowser/21.5.0.582 Yowser/2.5 Safari/537.36"
 }
 
 url = "https://yugenmangas.com/"
@@ -15,20 +15,27 @@ soup = BeautifulSoup(response.text, features='lxml')
 divPadre = soup.find('div', 'col-md-8 col-sm-12 col-xs-12')
 divSeries = divPadre.findAll('div', 'd-flex justify-content-center')
 contenedorSeries = divSeries[1]
-listaSeries = contenedorSeries.findAll('div', 'col-6 col-sm-6 col-md-6 col-xl-3')
+listaSeries = contenedorSeries.findAll(
+    'div', 'col-6 col-sm-6 col-md-6 col-xl-3')
 
 
 for serie in listaSeries:
-    #Obtener Imagen Serie
-    contenedorImagen = BeautifulSoup(str(serie.find('img', class_='series-image')), 'html.parser').img
+    # Obtener Imagen Serie
+    contenedorImagen = BeautifulSoup(
+        str(serie.find('img', class_='series-image')), 'html.parser').img
     urlImagen = contenedorImagen['src']
 
     print(urlImagen)
 
-    #obtener Nombre y Url de la serie
+    # obtener Nombre y Url de la serie
     contenedorPadre = serie.find('div', 'series-content')
     contenedorEnlace = BeautifulSoup(str(serie.find('a')), 'html.parser').a
     enlace = contenedorEnlace['href']
+
+    contenedorTitulo = BeautifulSoup(
+        str(contenedorPadre.find('h5')), 'html.parser').h5.text
+
     print(enlace)
-    print(enlace[:enlace.index('/{3}?')])
+    # print(enlace[:enlace.index('/{3}?')])
+    print(contenedorTitulo)
     print()
