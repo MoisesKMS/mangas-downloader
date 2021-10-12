e6a19eef40a20da26417310ef4a890204b027135
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-10-12 14:13:08 -0500

    1.3.1
    -Optimizado el código para obtener el estado y fecha de estreno
    -Optimizado el código para obtener el titulo alternativo

--- serie.py
+++ serie.py
@@ -1,12 +1,11 @@
 from Motor import Motor
-from bs4 import BeautifulSoup, NavigableString
-
 
 print('Ingresa la URL de una serie')
 url = input()
 soup = Motor.navegar(url)
 
 divPadre = soup.find('div', 'site-content')
+print()
 
 # Nombre
 titulo = divPadre.find('div', 'post-title').text.strip()
@@ -16,45 +15,64 @@ print('Titulo: ', titulo)
 imagen = divPadre.find('div', 'summary_image').find('img')['data-src']
 print('Imagen: ', imagen)
 
-# Descripcion
-descripcion = divPadre.find('div', 'summary__content').text.strip()
-print('Descripcion: ', descripcion)
-
-
 ## Obtenemos el contenedor para el Nombre original y Generos ##
 contenedorInfo = divPadre.find('div', 'post-content')
 contenedorInfo = contenedorInfo.find_all('div', 'summary-content')
 
 
 # Nombre Original
-tituloOriginal = contenedorInfo[2].text.strip()
-print('Titulo Original: ', tituloOriginal)
+# Obtenemos todos los heading de la info, y despues comprobamos que exista el texto 'Alternativa' para mostrar el titulo alternativo
+# En caso de no existir, se asignara el titulo principal al Alternativo
+divsHeading = divPadre.find_all('div', 'summary-heading')
 
+if(divsHeading[2].text.strip() == 'Alternativa'):
+    tituloOriginal = contenedorInfo[2].text.strip()
+else:
+    tituloOriginal = titulo
+
+print('Titulo Original: ', tituloOriginal)
 print()
+
+# Descripcion
+descripcion = divPadre.find('div', 'summary__content').text.strip()
+print('Descripcion: ', descripcion)
+print()
+
 # Generos
-divGeneros = contenedorInfo[3].find('div', 'genres-content')
+divGeneros = divPadre.find('div', 'genres-content')
 generos = divGeneros.find_all('a')
+
 for genero in generos:
     nombreGenero = genero.text
     urlGenero = genero['href']
     print('Genero: ', nombreGenero)
     print('Enlace: ', urlGenero)
 
-
 print()
-## Obtenemos el estado y la fecha ##
+
+
+## Obtenemos el estado y la fecha busando todos los divs que tenga la clase sumary-content para despues iterar sobre ellos ##
 contenedorEstado = divPadre.find('div', 'post-status')
 contenedorEstado = contenedorEstado.find_all('div',  'summary-content')
-# Estado
-fecha = contenedorEstado[0].text.strip()
-estado = contenedorEstado[1].text.strip()
-print('Fecha: ', fecha)
-print('Estado: ', estado)
+
+# Taratamos de obtner tanto el estado como la fecha de estreno, sino no hay fecha de estreno solo mostramos el estado
+# En un caso exitoso contenedorEstado[0] tiene la fecha y contenedorEstado[1] el estado de emision
+try:
+    fecha = contenedorEstado[0].text.strip()
+    estado = contenedorEstado[1].text.strip()
+    print('Fecha: ', fecha)
+    print('Estado: ', estado)
+except:
+    estado = contenedorEstado[0].text.strip()
+    print('Estado: ', estado)
 
 print()
+
+
 # [Lista de Capitulos]
 listaCapitulos = soup.find_all('li', 'wp-manga-chapter')
 print('Capitulos Encontrados: ', len(listaCapitulos))
+print()
 for capitulo in listaCapitulos:
     # Nombre del Capitulo
     nombreCapitulo = capitulo.find('a').text.strip()
