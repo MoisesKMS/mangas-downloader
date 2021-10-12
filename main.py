0a4798c4f033a478c1231962a4e7d8437f7c5d77
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-10-12 00:22:25 -0500

    1.2.1
    Optimizado el código para obtener capítulos recientes

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from Motor import Motor
 
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.41 YaBrowser/21.5.0.582 Yowser/2.5 Safari/537.36"
-}
 
-url = "https://yugenmangas.com/"
-
-# hacemos que la solicitud vaya con nuestro user agent
-response = requests.get(url, headers=headers)
-response.encoding = 'utf-8'
-# guardamos la respuesta de la solicitud en 'soup'
-soup = BeautifulSoup(response.text, features='lxml')
+soup = Motor.navegar("https://yugenmangas.com/")
 
 contenedorSeries = soup.find_all('div', 'card series')
 
 for serie in contenedorSeries:
+    # url
+    url = serie.find('a')['href'].split('/')
+    url = f'{url[0]}//{url[2]}/{url[3]}/{url[4]}/'
+
     # nombre
     titulo = serie.find('a')['href'].split(
         '/')[4].replace('-', ' ').capitalize().strip()
-    # if(str.isdigit(tiutlo) != True):
+
+    # si el nombre de la serie regresa numeros accedemos a la url de la serie para obtener el titulo
+    if(str.isdigit(titulo)):
+        soup = Motor.navegar(url)
+        divPadre = soup.find('div', 'site-content')
+        titulo = divPadre.find('div', 'post-title').text.strip()
 
     # numero ultimo capitulo
     numeroCapitulo = serie.find(
@@ -27,12 +28,6 @@ for serie in contenedorSeries:
 
     # imagen
     imagenUrl = serie.find('img')['src']
-
-    # url
-    url = serie.find('a')['href'].split(
-        '/')
-
-    url = f'{url[0]}//{url[2]}/{url[3]}/{url[4]}/'
 
     print(titulo)
     print(numeroCapitulo)
